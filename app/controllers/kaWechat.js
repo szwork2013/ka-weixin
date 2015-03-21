508d3533aba862ac0b21987d1a5c0e73b6668eac
linzehuan <linzehuan@163.com>
2015-03-21 12:22:24 +0800

    fiX a reply

--- app/controllers/kaWechat.js
+++ app/controllers/kaWechat.js
@@ -78,7 +78,7 @@ exports.index = wechat(config, wechat.text(function (message, req, res) {
   } else if (message.Event === 'CLICK') {
     var content = '';
     switch(message.EventKey){
-        case 'recharge': content = '点击我的微信：&lt;a href=&quot;weixin://contacts/profile/linzehuan_&quot;&gt;StartOne&lt;/a&gt';break;
+        case 'recharge': content = '点击我的微信：&lt;a href=&quot;weixin://contacts/profile/linzehuan_&quot;&gt;StartOne&lt;/a&gt;';break;
         default: content = 'click类型不存在';
       };
     res.reply(content); 
